3ac4b9372d85f4e38ba56a56ac7b2c3e2827c222
dubbs1515 <dubbsware@gmail.com>
2021-05-09 18:19:20 -0700

    app setup

--- server.R
+++ server.R
@@ -1,4 +1,8 @@
-## server.R
+##############################################################################
+# server.R
+##############################################################################
+
+source('functions/helpers.R')
 
 library(dplyr)
 library(ggplot2)
@@ -8,24 +12,21 @@ library(data.table)
 library(reshape2)
 library(Matrix)
 
-source('functions/helpers.R')
 
-# Code for System 1
-# define functions
+# DEFINE FUNCTION
 get_user_ratings = function(value_list) {
-  dat <- data.table(MovieID = sapply(strsplit(names(value_list), "_"),
-                                    function(x) ifelse(length(x) > 1, x[[2]], NA)),
-                   Rating = unlist(as.character(value_list)))
-  # dat = data.table(MovieID = names(value_list),Rating = unlist(as.character(value_list)))
-  dat <- dat[!is.null(Rating) & !is.na(MovieID)]
-  dat[Rating == " ", Rating := 0]
-  dat[, ':=' (MovieID = as.numeric(MovieID), Rating = as.numeric(Rating))]
-  dat <- dat[Rating > 0]
-  dat[,MovieID := paste0('m', MovieID)]
-
+    dat = data.table(MovieID = sapply(strsplit(names(value_list), "_"),
+                                       function(x) ifelse(length(x) > 1, x[[2]], NA)),
+                      Rating = unlist(as.character(value_list)))
+    dat = dat[!is.null(Rating) & !is.na(MovieID)]
+    dat[Rating == " ", Rating := 0]
+    dat[, ':=' (MovieID = as.numeric(MovieID), Rating = as.numeric(Rating))]
+    dat = dat[Rating > 0]
+    dat[,MovieID := paste0('m', MovieID)]
+    
 }
 
-# read in data
+# READ DATA
 myurl = "https://liangfgithub.github.io/MovieData/"
 movies = readLines(paste0(myurl, 'movies.dat?raw=true'))
 movies = strsplit(movies, split = "::", fixed = TRUE, useBytes = TRUE)
@@ -45,169 +46,181 @@ ratings = read.csv(paste0(myurl, 'ratings.dat?raw=true'),
                    header = FALSE)
 colnames(ratings) = c('UserID', 'MovieID', 'Rating', 'Timestamp')
 
-## Ratings per Movie
-ratings_m = ratings %>% 
-  group_by(MovieID) %>% 
-  summarize(ratings_per_movie = n(), ave_ratings = mean(Rating)) %>%
-  inner_join(movies, by = 'MovieID')
+rate_per_movie = ratings %>% 
+    group_by(MovieID) %>% 
+    summarize(ratings_per_movie = n(), ave_ratings = mean(Rating)) %>%
+    inner_join(movies, by = 'MovieID')
 
-## Create genre matrix
+## GENERATE GENRE MATRIX
 genres = as.data.frame(movies$Genres, stringsAsFactors=FALSE)
 tmp = as.data.frame(tstrsplit(genres[,1], '[|]',
                               type.convert=TRUE),
                     stringsAsFactors=FALSE)
+
+# AVAILABLE GENRES
 genre_list = c("Action", "Adventure", "Animation", 
                "Children's", "Comedy", "Crime",
                "Documentary", "Drama", "Fantasy",
                "Film-Noir", "Horror", "Musical", 
                "Mystery", "Romance", "Sci-Fi", 
                "Thriller", "War", "Western")
+
 m = length(genre_list)
 genre_matrix = matrix(0, nrow(movies), length(genre_list))
+
 for(i in 1:nrow(tmp)){
-  genre_matrix[i,genre_list %in% tmp[i,]]=1
+    genre_matrix[i,genre_list %in% tmp[i,]]=1
 }
+
 colnames(genre_matrix) = genre_list
 rownames(genre_matrix) = movies$MovieID
 remove("tmp", "genres")
 
-#Code for System 2
-## Recommender System
 
-# Create rating matrix
+# CREATE REAL RATING MATRIX
 i = paste0('u', ratings$UserID)
 j = paste0('m', ratings$MovieID)
 x = ratings$Rating
-tmp = data.frame(i, j, x, stringsAsFactors = T)
+tmp = data.frame(i, j, x, stringsAsFactors = TRUE)
 Rmat = sparseMatrix(as.integer(tmp$i), as.integer(tmp$j), x = tmp$x)
 rownames(Rmat) = levels(tmp$i)
 colnames(Rmat) = levels(tmp$j)
 Rmat = new('realRatingMatrix', data = Rmat)
 
-# Create the recommenders
-e <- evaluationScheme(Rmat, method="split", train=0.9, given=3, goodRating=5)
-r1 <- Recommender(getData(e, "train"), "UBCF", parameter = list(normalize = 'center',
-                                                                method = 'Cosine', nn = 50))
+# GENERATE RECOMMENDER (via recommenderlab package)
+evalScheme = evaluationScheme(Rmat, method="split", train=0.9, given=3, goodRating=4)
+recommender = Recommender(getData(evalScheme, "train"),
+                          "UBCF", parameter = list(normalize = 'center',
+                                                   method = 'Cosine', nn = 50))
 
-## Shiny Server
- 
+##############################################################################
+# SHINY SERVER
+##############################################################################
 shinyServer(function(input, output, session) {
-  
-  ## System 1
-  
-  #exact data from dataset
-  datasetInput <- reactive({input$dataset})  
-  
-  # Calculate recommendations when the sbumbutton is clicked
-  df1 <- eventReactive(input$btn1, {
-    dataset = datasetInput()
-
-    # get the movieID (Row in Movie)
-    movielist = rownames(genre_matrix[genre_matrix[,dataset] == 1,])
-    # Rank the movies based on their popularities
-    genre_movies <- ratings_m %>% filter(MovieID %in% movielist) 
-    genre_movies <- genre_movies %>% arrange(desc(ratings_per_movie))
-    data.table(Number = 1:20, MovieID = genre_movies$MovieID[1:20], Title = genre_movies$Title[1:20])
     
-  }) # clicked on button
-
-  # display the recommendations
-  output$results1 <- renderUI({
-    num_rows <- 4
-    num_movies <- 5
-    recom_result <- df1()
+    ############
+    # SYSTEM 1
+    ############
     
-    lapply(1:num_rows, function(i) {
-      list(fluidRow(lapply(1:num_movies, function(j) {
-        box(width = 2, status = "success", solidHeader = TRUE, title = paste0("Number ", (i - 1) * num_movies + j),
-            
-            div(style = "text-align:center", 
-                a(img(src = movies$image_url[which(movies$MovieID==recom_result$MovieID[(i - 1) * num_movies + j])], height = 150))
-            ),
-            div(style="text-align:center; font-size: 100%", 
-                strong(movies$Title[which(movies$MovieID==recom_result$MovieID[(i - 1) * num_movies + j])])
-            )
-            
-        )        
-      }))) # columns
-    }) # rows
+    # ASSIGN DATA
+    datasetInput = reactive({input$dataset})  
     
-  }) # renderUI function
-  
-  ## System 2
-
-  # show the movies to be rated
-  output$ratings <- renderUI({
-    num_rows <- 40
-    num_movies <- 6 # movies per row
-
-    lapply(1:num_rows, function(i) {
-      list(fluidRow(lapply(1:num_movies, function(j) {
-        list(box(width = 2,
-                 div(style = "text-align:center", img(src = movies$image_url[(i - 1) * num_movies + j], height = 150)),
-                 div(style = "text-align:center", strong(movies$Title[(i - 1) * num_movies + j])),
-                 div(style = "text-align:center; font-size: 150%; color: #f0ad4e;", ratingInput(paste0("select_", movies$MovieID[(i - 1) * num_movies + j]), label = "", dataStop = 5)))) #00c0ef
-      })))
+    # CALCULATE RECOMMENDATIONS
+    recom1 = eventReactive(input$btn1, {
+        dataset = datasetInput()
+        
+        # SELECT MOVIE ID
+        movielist = rownames(genre_matrix[genre_matrix[,dataset] == 1,])
+        
+        # RANK MOVIES BY POPULARITY
+        genre_movies = rate_per_movie %>% filter(MovieID %in% movielist) 
+        genre_movies = genre_movies %>% arrange(desc(ratings_per_movie))
+        data.table(Number = 1:15, MovieID = genre_movies$MovieID[1:15], 
+                   Title = genre_movies$Title[1:15])
+        
+    }) # BUTTON CLICKED
+    
+    # DISPLAY RESULTS
+    output$results1 = renderUI({
+        num_rows = 3
+        num_movies = 5
+        recom_result = recom1()
+        
+        lapply(1:num_rows, function(i) {
+            list(fluidRow(lapply(1:num_movies, function(j) {
+                box(width = 2, status = "success", solidHeader = TRUE, 
+                    title = paste0("#", (i - 1) * num_movies + j),
+                    
+                    div(style = "text-align:center", 
+                        a(img(src = movies$image_url[which(movies$MovieID==recom_result$MovieID[(i - 1) * num_movies + j])], 
+                              height = 150))
+                    ),
+                    div(style="text-align:center; font-size: 100%", 
+                        strong(movies$Title[which(movies$MovieID==recom_result$MovieID[(i - 1) * num_movies + j])])
+                    )
+                    
+                )        
+            }))) # COLUMN
+        }) # ROW
+        
+    }) # renderUI FUNCTION
+    
+    ############
+    # SYSTEM 2
+    ############
+    
+    # DISPLAY MOVIES TO RATE
+    output$ratings = renderUI({
+        num_rows = 40
+        num_movies = 6 # movies per row
+        
+        lapply(1:num_rows, function(i) {
+            list(fluidRow(lapply(1:num_movies, function(j) {
+                list(box(width = 2,
+                         div(style = "text-align:center", img(src = movies$image_url[(i - 1) * num_movies + j], height = 150)),
+                         div(style = "text-align:center", strong(movies$Title[(i - 1) * num_movies + j])),
+                         div(style = "text-align:center; font-size: 150%; color: #f0ad4e;", ratingInput(paste0("select_", movies$MovieID[(i - 1) * num_movies + j]), label = "", dataStop = 5)))) #00c0ef
+            })))
+        })
     })
-  })
-
-  # Calculate recommendations when the sbumbutton is clicked
-  df2 <- eventReactive(input$btn2, {
-    withBusyIndicatorServer("btn2", { # showing the busy indicator
-      # hide the rating container
-      useShinyjs()
-      jsCode <- "document.querySelector('[data-widget=collapse]').click();"
-      runjs(jsCode)
-
-      # get the user's rating data
-      value_list <- reactiveValuesToList(input)
-      user_ratings <- get_user_ratings(value_list)
-      m_new <- sort(unique(paste0('m', ratings$MovieID)))
-      u_new <- rep("u000", length(m_new))
-      r_new <- rep(0, length(m_new))
-      Rnew = data.table(UserID=u_new, MovieID=m_new, Rating=r_new, stringsAsFactors = T)
-      # write.table(Rnew, file = 'rec.txt', sep = ',')
-      Rnew[MovieID %in% user_ratings$MovieID, Rating:= as.numeric(user_ratings$Rating)]
-      Rnew[Rating == 0, Rating := NA]
-      # write.table(Rnew, file = 'rec2.txt', sep = ',')
-      RSnew = matrix(Rnew$Rating, ncol=length(m_new), dimnames=list(UserID="u000", MovieID=Rnew$MovieID))
-      RSnew <- as(RSnew,"realRatingMatrix")
-
-      movielist <- predict(r1, RSnew, type="topNList", n=20)
-      movielist <- as(movielist,"list")[[1]]
-      movielist <- as.numeric(lapply(movielist, function(x) substr(x, 2, nchar(x))))
-
-      # Rank the movies based on their popularities
-      rec_movies <- ratings_m %>% filter(MovieID %in% movielist) 
-      rec_movies <- rec_movies %>% arrange(desc(ratings_per_movie))
-      data.table(Number = 1:20, MovieID = rec_movies$MovieID[1:20], Title = rec_movies$Title[1:20])
-      
-    }) # still busy
-
-  }) # clicked on button
-
-
-  # display the recommendations
-  output$results2 <- renderUI({
-    num_rows <- 4
-    num_movies <- 5
-    recom_result <- df2()
-
-    lapply(1:num_rows, function(i) {
-      list(fluidRow(lapply(1:num_movies, function(j) {
-        box(width = 2, status = "success", solidHeader = TRUE, title = paste0("Rank ", (i - 1) * num_movies + j),
-
-            div(style = "text-align:center",
-                a(img(src = movies$image_url[which(movies$MovieID==recom_result$MovieID[(i - 1) * num_movies + j])], height = 150))
-            ),
-            div(style="text-align:center; font-size: 100%",
-                strong(movies$Title[which(movies$MovieID==recom_result$MovieID[(i - 1) * num_movies + j])])
-            )
-
-        )
-      }))) # columns
-    }) # rows
-
-  }) # renderUI function
-  
-}) # server function
+    
+    # CALCULATE RECOMMENDATIONS
+    recom2 = eventReactive(input$btn2, {
+        withBusyIndicatorServer("btn2", { # showing the busy indicator
+            # COLLAPSE RATINGS
+            useShinyjs() # EXTEND SHINY TO USE CUSTOM JS
+            runjs("document.querySelector('[data-widget=collapse]').click();")
+            
+            # GET USER RATINGS
+            value_list = reactiveValuesToList(input)
+            user_ratings = get_user_ratings(value_list)
+            
+            movie_new = sort(unique(paste0('m', ratings$MovieID)))
+            user_new = rep("u000", length(movie_new))
+            rate_new = rep(0, length(movie_new))
+            R_new = data.table(UserID=user_new, MovieID=movie_new, Rating=rate_new, stringsAsFactors = TRUE)
+            R_new[MovieID %in% user_ratings$MovieID, Rating:= as.numeric(user_ratings$Rating)]
+            R_new[Rating == 0, Rating := NA]
+            RRM_new = matrix(R_new$Rating, ncol=length(movie_new), dimnames=list(UserID="u000", MovieID=R_new$MovieID))
+            RRM_new = as(RRM_new,"realRatingMatrix")
+            
+            movielist = predict(recommender, RRM_new, type="topNList", n=20)
+            movielist = as(movielist,"list")[[1]]
+            movielist = as.numeric(lapply(movielist, function(x) substr(x, 2, nchar(x))))
+            
+            # RANK MOVIES BY POPULARITY
+            rec_movies = rate_per_movie %>% filter(MovieID %in% movielist) 
+            rec_movies = rec_movies %>% arrange(desc(ratings_per_movie))
+            data.table(Number = 1:20, MovieID = rec_movies$MovieID[1:20], Title = rec_movies$Title[1:20])
+            
+        }) # BUSY
+        
+    }) # BUTTON CLICKED
+    
+    
+    # DISPLAY RECOMMENDATIONS
+    output$results2 = renderUI({
+        num_rows = 4
+        num_movies = 5
+        recom_result = recom2()
+        
+        lapply(1:num_rows, function(i) {
+            list(fluidRow(lapply(1:num_movies, function(j) {
+                box(width = 2, status = "success", solidHeader = TRUE, 
+                    title = paste0("#", (i - 1) * num_movies + j),
+                    
+                    div(style = "text-align:center",
+                        a(img(src = movies$image_url[which(movies$MovieID==recom_result$MovieID[(i - 1) * num_movies + j])],
+                              height = 150))
+                    ),
+                    div(style="text-align:center; font-size: 100%",
+                        strong(movies$Title[which(movies$MovieID==recom_result$MovieID[(i - 1) * num_movies + j])])
+                    )
+                    
+                )
+            }))) # COLUMN
+        }) # ROW
+        
+    }) # renderUI FUNCTION
+    
+}) # SERVER FUNCTION
--- ui.R
+++ ui.R
@@ -1,4 +1,6 @@
-## ui.R
+##############################################################################
+# ui.R
+##############################################################################
 
 library(shiny)
 library(shinydashboard)
@@ -11,74 +13,73 @@ source('functions/helpers.R')
 
 shinyUI(
     dashboardPage(
-          skin = "blue",
-          dashboardHeader(title = "Movie Recommender"),
-          
-          dashboardSidebar(
-              sidebarMenu(
-                menuItem("Recommend by Genre", tabName = "genres", icon = icon("film")),
-                menuItem("Recommend by Rating", tabName = "rating", icon = icon("star"))
-              )
-          ),
-
-          dashboardBody(includeCSS("css/movies.css"),
-              tabItems(
-                # First tab content
-                tabItem(tabName = "genres",
-                  fluidRow(
-                      box(width = 12, title = "Step 1: Select your favorite genre", status = "info", solidHeader = TRUE, collapsible = FALSE,
-                          selectInput(inputId = "dataset",
-                                      label = "Choose a genre",
-                                      choices = c("Action", "Adventure", "Animation", 
-                                                     "Children's", "Comedy", "Crime",
-                                                     "Documentary", "Drama", "Fantasy",
-                                                     "Film-Noir", "Horror", "Musical", 
-                                                     "Mystery", "Romance", "Sci-Fi", 
-                                                     "Thriller", "War", "Western")
+        skin = "blue",
+        dashboardHeader(title = "Movie Recommender"),
+        
+        dashboardSidebar(
+            sidebarMenu(
+                menuItem("MOVIES BY GENRE", tabName = "genres", icon = icon("ticket-alt")),
+                menuItem("MOVIES BY RATING", tabName = "rating", icon = icon("grin-stars"))
+            )
+        ),
+        
+        dashboardBody(includeCSS("css/movies.css"),
+                      tabItems(
+                          # TAB 1: RECOMMENDATIONS BY GENRE
+                          tabItem(tabName = "genres",
+                                  fluidRow(
+                                      box(width = 12, title = "Step 1: Select Genre", status = "info", 
+                                          solidHeader = TRUE, collapsible = FALSE,
+                                          selectInput(inputId = "dataset",
+                                                      label = "Select Genre",
+                                                      choices = c("Action", "Adventure", "Animation", 
+                                                                  "Children's", "Comedy", "Crime",
+                                                                  "Documentary", "Drama", "Fantasy",
+                                                                  "Film-Noir", "Horror", "Musical", 
+                                                                  "Mystery", "Romance", "Sci-Fi", 
+                                                                  "Thriller", "War", "Western")
+                                          ),
+                                      )
+                                  ),
+                                  fluidRow(
+                                      useShinyjs(),
+                                      box(
+                                          width = 12, status = "info", solidHeader = TRUE,
+                                          title = "Step 2: Discover movies you might like",
+                                          br(),
+                                          withBusyIndicatorUI(
+                                              actionButton("btn1", "GO", class = "btn-secondary", icon = icon("search"))
+                                          ),
+                                          br(),
+                                          tableOutput("results1")
+                                      )
+                                  )
                           ),
+                          
+                          # TAB 2: RECOMMENDATIONS BY RATINGS
+                          tabItem(tabName = "rating",
+                                  fluidRow(
+                                      box(width = 12, title = "Step 1: Rate Movies", status = "info", solidHeader = TRUE, collapsible = TRUE,
+                                          div(class = "rateitems",
+                                              uiOutput('ratings')
+                                          )
+                                      )
+                                  ),
+                                  fluidRow(
+                                      useShinyjs(),
+                                      box(
+                                          width = 12, status = "info", solidHeader = TRUE,
+                                          title = "Step 2: Get Movies Recommended For You",
+                                          br(),
+                                          withBusyIndicatorUI(
+                                              actionButton("btn2", "GO", class = "btn-secondary", icon = icon("search"))
+                                          ),
+                                          br(),
+                                          tableOutput("results2")
+                                      )
+                                  )
+                          )
                       )
-                  ),
-                  fluidRow(
-                      useShinyjs(),
-                      box(
-                        width = 12, status = "info", solidHeader = TRUE,
-                        title = "Step 2: Discover movies you might like",
-                        br(),
-                        withBusyIndicatorUI(
-                          actionButton("btn1", "Click here to get your recommendations", class = "btn-warning")
-                        ),
-                        br(),
-                        tableOutput("results1")
-                      )
-                  )
-                ),
-                
-                # Second tab content
-                tabItem(tabName = "rating",
-                  fluidRow(
-                    box(width = 12, title = "Step 1: Rate as many movies as possible (at least 10 is recommended)", status = "info", solidHeader = TRUE, collapsible = TRUE,
-                        div(class = "rateitems",
-                            uiOutput('ratings')
-                        )
-                    )
-                  ),
-                  fluidRow(
-                    useShinyjs(),
-                    box(
-                      width = 12, status = "info", solidHeader = TRUE,
-                      title = "Step 2: Discover movies you might like",
-                      br(),
-                      withBusyIndicatorUI(
-                        actionButton("btn2", "Click here to get your recommendations", class = "btn-warning")
-                      ),
-                      br(),
-                      tableOutput("results2")
-                    )
-                  )
-                )
-                
-                
-              )
-          )
+        )
     )
 ) 
